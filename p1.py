18d09ad301792ac6c98e00066bd1c0a70e73ad1a
wysockey <twysockey@sjnma.org>
2018-02-06 14:42:32 -0600

    homework1.0

---
+++ p1.py
@@ -0,0 +1,25 @@
+
+
+
+#Integer
+
+Me69 = 40.0
+
+Me96 = 20.0
+
+#Floating point
+
+dank69 = Me69 / Me96
+
+#String
+print(dank69)
+
+#boolean
+
+Dankme-mes = False
+
+Dankmemes  = True
+
+
+
+#watch video about boolean
