d83b1691c965289f022e2fa71a9e4813cd256c60
wysockey <twysockey@sjnma.org>
2018-03-12 09:39:11 -0500

    Computer homework

---
+++ hw6/Gulags.py
@@ -0,0 +1,132 @@
+#Gulags keep the population down
+
+bowl=raw_input("Do you want to bowl?")
+
+yes= raw_input()
+if ("Yes") or("yes")
+	print ("lets start bowling!")
+	No= raw_input()
+	elif ("No") or ("no")
+		print("Then why bowl?")
+#since user said yes start bowling and asking them their score.
+# Note score can not excede over ten points per turn
+#PPT = points per turn
+#TPPT = total points per turns
+#Index is used as a way to prevent cheating and to make the user restart the game
+x = 10
+loopCount = 10
+index = 0
+index = index - 1
+ppt= 10
+while (index < loopCount):
+
+elif(input =<  ppt):
+	print("Go again")
+elif(input =  ppt):
+	print("Strike")
+elif(input + input = ppt):
+	print ("Spair!")
+elif(input + input > ppt)
+	print (index)
+#first turn
+turn0 = raw_input()
+
+turn01 = raw_input()
+
+tppt= turn0 +  turn01
+
+print (tppt)
+
+#second turn
+
+
+
+turn2 = raw_input()
+
+turn3 = raw_input()
+
+tpptt= turn0 +  turn01 + turn2 + turn 3
+
+print (tpptt)
+
+#third turn
+
+
+turn4 = raw_input()
+
+turn5 = raw_input()
+
+tppttt= turn0 +  turn01 + turn2 + turn3 + turn4 + turn5
+
+print (tppttt)
+
+#fourth turn
+
+turn6 = raw_input()
+
+turn7 = raw_input()
+
+tpptttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7
+print (tppttt)
+#fifth turn
+turn8 = raw_input()
+
+turn9 = raw_input()
+
+tppttttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9
+
+print (tpptttt)
+#sixth turn
+
+turn10 = raw_input()
+
+turn11= raw_input()
+
+tppttttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9+turn 10 +turn11
+
+print (tppttttt)
+
+#7
+
+turn12 = raw_input()
+
+turn13= raw_input()
+
+tpptttttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9+turn10 +turn11+ turn12+turn13 
+
+print (tpptttttt)
+
+
+#8
+
+turn14 = raw_input()
+
+turn15= raw_input()
+
+tpptttttrt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9+turn10 +turn11+ turn12+turn13 +turn14+turn15
+
+print (tppttttttt)
+
+
+#9
+turn17 = raw_input()
+
+turn16= raw_input()
+
+tpptttttttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9+turn10 +turn11+ turn12+turn13 +turn14+turn15+turn16+turn17
+
+print (tpptttttttt)
+
+
+#ten
+
+turn18 = raw_input()
+
+turn19= raw_input()
+
+tppttttttttt= turn0 +  turn01 + turn2 + turn 3 + turn 4+ turn5 + turn6 +turn7+turn8 +turn9+turn10 +turn11+ turn12+turn13 +turn14+turn15+turn16+turn17+turn18+turn19
+
+print (tppttttttttttt)
+
+
+
